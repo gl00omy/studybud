20581a944374942414fc6c6d7b408fc12e13a0be
Zhibek Tyulemissova <zhibektyulemissova@gmail.com>
2023-03-14 16:40:27 +0100

    admin&CREATE

--- base/views.py
+++ base/views.py
@@ -1,22 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .models import Room
+from .forms import RoomForm
 
-rooms = [
-    {'id':1, 'name':'Lets learn python'}, 
-    {'id':2, 'name':'Design with me'}, 
-    {'id':3, 'name':'Frontend developers'}, 
-]
+# rooms = [
+#     {'id':1, 'name':'Lets learn python'}, 
+#     {'id':2, 'name':'Design with me'}, 
+#     {'id':3, 'name':'Frontend developers'}, 
+# ]
 
 # Create your views here.
 def home(request):
+    rooms = Room.objects.all()
     context = {'rooms': rooms}
     return render(request, 'base/home.html', context)
 
 def room(request, pk):
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
+    room = Room.objects.get(id=pk)
     context = {'room': room}
 
     return render(request, 'base/room.html', context)
+
+def createRoom(request):
+    form = RoomForm()
+    if request.method == 'POST':
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    context = {'form': form}
+    return render(request, 'base/room_form.html', context)
