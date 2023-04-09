7f8e189c250acf0978c2fd2ad9f0b95f522754ff
Zhibek Tyulemissova <zhibektyulemissova@gmail.com>
2023-04-09 20:50:43 +0200

    user login form

--- base/views.py
+++ base/views.py
@@ -10,6 +10,10 @@ from .forms import RoomForm
 #     {'id':3, 'name':'Frontend developers'}, 
 # ]
 
+def loginPage(request):
+    context = {}
+    return render(request, 'base/login_register.html', context)
+
 # Create your views here.
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
